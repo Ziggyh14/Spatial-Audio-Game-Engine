0ae5fd44ba935f81777a9dbf6b3cb1a63ab4272c
psyhs10 <psyhs10@exmail.nottingham.ac.uk>
2023-10-18 18:04:43 +0100

    seperate main function into two new 'init' and 'close' functions for SDL windows

--- src/hello.c
+++ src/hello.c
@@ -1,10 +1,42 @@
-#include <SDL2/SDL.h>
-#include <stdio.h>
+#include "hello.h"
+
+
+SDL_Window* init_Window (const char* title, int width, int height){
+
+    if(SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO)){
+        printf("error initializing SDL: %s\n", SDL_GetError());
+        return NULL;
+        }
+
+    return SDL_CreateWindow(
+            title,
+            SDL_WINDOWPOS_CENTERED_DISPLAY(0),
+            SDL_WINDOWPOS_CENTERED_DISPLAY(0),
+            width,
+            height,
+            SDL_WINDOW_ALLOW_HIGHDPI);
+}
+
+void close_Window(SDL_Window* window){
+
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+}
+
 
 int main(int argc, char** argv){
 
-    printf("hello, world!");
+    SDL_Window* window = init_Window("DEMO", 1280,720);
+
+    SDL_Event e;
+    int quit = 0;
+    while(!quit){
+        while(SDL_PollEvent(&e)){
+            if(e.type == SDL_QUIT){
+                quit = 1;
+            }
+        }
+    }
+    close_Window(window);
     return 0;
-
-
 }
