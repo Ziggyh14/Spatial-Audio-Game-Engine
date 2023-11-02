3a37d76bcd871d9a1793fe5c71106e0d324f6cd3
psyhs10 <psyhs10@exmail.nottingham.ac.uk>
2023-11-02 17:44:36 +0000

    add comments for state_manager.h

--- src/state_manager.h
+++ src/state_manager.h
@@ -24,13 +24,39 @@ typedef struct StateManager{
 
 } StateManager;
 
+/**
+ * Create new state manager return a pointer to it
+ * return null on error
+*/
 StateManager* init_StateManager(int capacity);
+/**
+ * Free statemanager and contents of stack
+*/
 void free_StateManager(StateManager* sm);
+/**
+ * Push a state to the stack,
+ *  returns result of state's init() function, -1 on error
+*/
 unsigned int push_State(State* s, StateManager* sm);
+/**
+ * Pop state from stack and free it
+ * Return result of that states destroy() function, -1 on error
+*/
 unsigned int pop_State(StateManager* sm);
+/**
+ * Peak at the state on the stop of the stack
+ * returns pointer to that state
+*/
 State* peak_State(StateManager* sm);
-
+/**
+ * Calls the update function of the state at the top of the stack
+ * returns the result of that function, -1 on error
+*/
 unsigned int update_StateManager(StateManager* sm, unsigned int dT);
+/**
+ * Calls the draw function of the state at the top of the stack
+ * returns the result of that function, -1 on error
+*/
 unsigned int draw_StateManager(StateManager* sm, unsigned int dT);
 
 
