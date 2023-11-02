b13a3c2e587d41ccefa6f3727e33ce455ec480cd
psyhs10 <psyhs10@exmail.nottingham.ac.uk>
2023-11-02 16:31:01 +0000

    add comments to main

--- src/main.c
+++ src/main.c
@@ -7,10 +7,11 @@
 #define FALSE 0
 #define ui64 uint64_t
 #define ui32 uint32_t
+#define PLAYER_VEL 0.5
 
 #define QUIT_CHECK if(SDL_QuitRequested()){break;}
 
-int playerpos = 0;
+float playerpos = 0;
 int check = 0;
 
 
@@ -20,19 +21,18 @@ ui32 update1(ui32 dT){
     while(pollEvent(&e)){
         if(isKeyDown(e)){
             switch(getKeyPressed(e)){
-                case KEY_UP: playerpos++; break;
-                case KEY_DOWN: playerpos--; break;
+                case KEY_UP: playerpos = playerpos + (PLAYER_VEL * dT); break;
+                case KEY_DOWN: playerpos = playerpos - (PLAYER_VEL * dT); break;
                 default: break;
             }
-            printf("%d\n",playerpos);
-
         }
     }
+    printf("%f\n",playerpos);
 
     return 0;
 }
 
-unsigned int draw1(){
+ui32 draw1(){
     if(playerpos == 50 && check == 0){
         play_Sample("bin/jazz.wav",0);
         check = 1;
@@ -48,29 +48,28 @@ int main(int argc, char** argv){
 
     create_Table(); // must be called before init_window
 
-    SDL_Window* w = init_Window("DEMO", 1280,720);
+    SDL_Window* w = init_Window("DEMO", 1280,720); //init sdl and create a window
 
-    init_Sample_Playback();
+    init_Sample_Playback(); //init sdl mixer, and reserve space for space queue
 
-    play_Sample("bin/jazz.wav",0);
+    //play_Sample("bin/jazz.wav",0); //play a sample from file, play loop+1 times
 
-    int running = TRUE;
-    ui32 lastTick = 0;
-    ui32 dT = 0;
+    ui32 lastTick = 0; // stores the last time for dT calc 
+    ui32 dT = 0; // variable for delta time used for physics calculations
 
-    while (running){
+    while (TRUE){ // game loop to run indefinitely unless quit
 
-        QUIT_CHECK;
+        QUIT_CHECK; // alias for detecting if a quit event is queued and breaks if there is
 
-        dT = SDL_GetTicks() - lastTick;
-        lastTick = SDL_GetTicks();
+        dT = SDL_GetTicks() - lastTick; // dT is time passed between now and completion of last frame
+        lastTick = SDL_GetTicks(); // set last tick for next loop
 
         //STATE FUNCTIONS
-        s.update(dT);
-        s.init();
+        s.update(dT); // update anything before "drawing" 
+       // s.draw(); // "draw" anything that needs drawing
     }
-    close_Sample_Playback();
-    close_Window(w);
+    close_Sample_Playback(); // close sample playback and delete the hash table
+    close_Window(w); // close window and exit SDL
     
 }
 
